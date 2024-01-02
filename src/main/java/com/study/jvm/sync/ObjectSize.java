1f273aa883e41b30cfee0cd81ea9d96367805bff
Williams <zhaojunyi@icc.link>
2024-01-02 18:05:33 +0800

    增加 pom 文件

---
+++ src/main/java/com/study/jvm/sync/ObjectSize.java
@@ -0,0 +1,14 @@
+package com.study.jvm.sync;
+
+import org.openjdk.jol.info.ClassLayout;
+
+/**
+ * @Author : Williams
+ * Date : 2024/1/2 17:49
+ */
+public class ObjectSize {
+    public static void main(String[] args) {
+        Object c= new Object();
+        System.out.println(ClassLayout.parseInstance(c).toPrintable());
+    }
+}
