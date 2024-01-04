af052342bd3366dd737139d0dc6e4233fb73bcda
Williams <zhaojunyi@icc.link>
2024-01-04 10:48:02 +0800

    增加打印对象信息

--- src/main/java/com/study/jvm/sync/ObjectSize.java
+++ src/main/java/com/study/jvm/sync/ObjectSize.java
@@ -9,6 +9,7 @@ import org.openjdk.jol.info.ClassLayout;
 public class ObjectSize {
     public static void main(String[] args) {
         Object c= new Object();
+        // 打印对象的信息
         System.out.println(ClassLayout.parseInstance(c).toPrintable());
     }
 }
