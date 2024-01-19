4f5d5a21e31c099cce886a66deaf8181322470b1
Williams <zhaojunyi@icc.link>
2024-01-19 14:53:13 +0800

    查看所有线程

---
+++ src/main/java/com/study/线程/查看所有线程/getThread.java
@@ -0,0 +1,20 @@
+package com.study.线程.查看所有线程;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
+
+/**
+ * @Author : Williams
+ * Date : 2024/1/19 14:51
+ */
+public class getThread {
+
+    public static void main(String[] args) {
+        ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
+        ThreadInfo[] threadInfos = threadMXBean.dumpAllThreads(false, false);
+        for ( ThreadInfo t :threadInfos){
+            System.out.println(t);
+        }
+    }
+}
